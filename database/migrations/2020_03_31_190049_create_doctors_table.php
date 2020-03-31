3dd7d853aaf9b8d77e20f471242c37a431afc0f2
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-03-31 21:46:20 +0200

    make migration of project

---
+++ database/migrations/2020_03_31_190049_create_doctors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('dr_name');
+            $table->string('password');
+            $table->string('dr_national_id');
+            $table->string('dr_avatar');
+            $table->integer('is_active')->default(1);
+            $table->integer('is_owner')->default(0);
+            $table->foreignId('pharmacy_id')->references('id')->on('pharmacies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
---
+++ database/migrations/2020_03_31_190251_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_name');
+            $table->string('Deliver_Address');
+            $table->integer('action')->default(0);
+            $table->integer('status')->default(1);
+            $table->foreignId('dr_id')->references('id')->on('pharmacies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
