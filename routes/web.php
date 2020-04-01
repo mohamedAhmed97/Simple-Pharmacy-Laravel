57302772bfffb1f0a6ee4688f5e4599223378bea
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-04-01 15:54:33 +0200

    added LTE Dashboard and authentication ui

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+//=====admins.index=========
+Route::get('/admins', 'AdminController@index')->name('admins.index');
