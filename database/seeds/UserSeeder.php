263d0d2aeaf4302efbc839b943c2cd50181c8d6e
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-04-01 20:10:58 +0200

    make user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //fill data of user
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        //create 100 user in DB
+        factory(App\User::class, 100)->create();
+    }
+}
