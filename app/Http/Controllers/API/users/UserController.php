6ab66a5c0e6bb31297ea990fe8dae24f36ff9a33
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-04-10 19:28:24 +0200

    fix conflict

--- app/Http/Controllers/API/users/UserController.php
+++ app/Http/Controllers/API/users/UserController.php
@@ -21,23 +21,10 @@ class UserController extends Controller
             'public/user',$user_avatar_name);
 
        $user=User::create([
-<<<<<<< HEAD
-        'name'=>$request->name,
-        'email'=>$request->email,
-        'password'=>Hash::make($request->password),
-        'phone'=>$request->phone,
-        'floor_number'=>$request->floor_number,
-        'flat_number'=>$request->flat_number,
-        'building_number'=>$request->building_number,
-        'street_id'=>$request->street_id,
-        'is_main'=>$request->is_main,
-        'area_id'=>$request->area_id,
-        'avatar'=>$user_avatar_name,
-=======
             'name'=>$request->name,
             'email'=>$request->email,
             'gender'=>$request->gender,
-            'password'=>$request->password, 
+            'password'=>Hash::make($request->password), 
             'date_of_birth'=>$request->date_of_birth,
             'avatar'=>$request->user_avatar_name,
             'mobile_number'=>$request->mobile_number,
@@ -48,7 +35,6 @@ class UserController extends Controller
             'floor_number'=>$request->floor_number,
             'flat_number'=>$request->flat_number,
             'is_main'=>$request->is_main,    
->>>>>>> fdf427d8e7ee87daca9729af37b56159d92228de
         ]);
         
         if($user)
