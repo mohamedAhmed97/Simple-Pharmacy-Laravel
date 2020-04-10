b5dbb6328c938622986d3dc213fb313df4578e55
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-04-10 17:45:59 +0200

    renamed API\UserController to API\UserCRUDController & added edit the user data  so all the CRUD that admin can do on users is done (Api)

--- app/Http/Controllers/API/UserCRUDController.php
+++ app/Http/Controllers/API/UserCRUDController.php
@@ -7,12 +7,14 @@ use Illuminate\Http\Request;
 use App\User;
 use App\Http\Resources\UserResource;
 
-class UserController extends Controller
+class UserCRUDController extends Controller
 {
+    //get all the users in the database (users table)
     public function index(){
         return UserResource::collection(User::all()); 
     }
 
+    //get one user in the database (users table)
     public function show(){
         $user= User::find(request()->user);
         //check if the user doesn't exist in the database
@@ -21,37 +23,32 @@ class UserController extends Controller
         }
         return new UserResource($user);
     }
-    public function store(){
-        $request = request();
-        $user=User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'gender'=>$request->gender,
-            'password'=>$request->password, 
-            'date_of_birth'=>$request->date_of_birth,
-            'avatar'=>$request->user_avatar_name,
-            'mobile_number'=>$request->mobile_number,
-            'national_id​'=>$request->national_id​,
-            'area_id'=>$request->area_id,
-            'street_name'=>$request->street_name,
-            'building_number'=>$request->building_number,
-            'floor_number'=>$request->floor_number,
-            'flat_number'=>$request->flat_number,
-            'is_main'=>$request->is_main,    
-        ]);
+
+    //add a new user to the database (users table)
+    public function store(Request $request){
+        $user=User::create($request->all());
         if($user)
         {
             return response()->json([
-                'Success' => 'User is added successfully ^_^ ',
-            ],200);
+                "Success" => 'User is added successfully ^_^ ',
+                "Data:"=> new UserResource($user),],200);
         }
         if(!$user){
             return response()->json(["Error"=>"Sorry,You can't create a new user as you have to fill all the required fields"],404);
         }
     }
 
+    //delete a user from the database (users table)
     public function destroy(){
         User::find(request()->user)->delete();
         return response()->json(["Success"=>"You deleted this user successfully, This record isn't a part of the database anymore"],200);
     }
+    
+    //update a user in the database (users table)
+    public function update(Request $request, User $user){ 
+        $user->update($request->all()); 
+        return response()->json(["Success"=>"User Updated successfully",
+                                    "New data:"=> new UserResource($user)],200);
+    }
+        
 }
